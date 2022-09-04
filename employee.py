4e9ad3144fa582e931b44ee1111cdea7ee4f7efd
Shreedhar <shreedharvpoojari1@gmail.com>
2022-09-05 01:40:12 +0530

    EMP update

--- employee.py
+++ employee.py
@@ -25,6 +25,7 @@ class EmpClass:
         self.var_emp_email = StringVar()
         self.var_emp_paswd = StringVar()
         self.var_emp_utype = StringVar()
+        self.var_emp_salary = StringVar()
 
 
         ##----Search Frame-----##
@@ -47,20 +48,83 @@ class EmpClass:
         lbl_gender = Label(self.root, text="Gender", font=("goudy old style", 15, "bold"), bg="#898AA6").place(x=450, y=170)
         lbl_contact = Label(self.root, text="Contact", font=("goudy old style", 15, "bold"), bg="#898AA6").place(x=900, y=170)
 
-        txt_empid = Entry(self.root, textvariable=self.var_emp_id, font=("goudy old style", 15, "bold"),bg="lightyellow").place(x=150, y=170,width=180)
+        txt_empid = Entry(self.root, textvariable=self.var_emp_id, font=("goudy old style", 15, "bold"),bg="lightyellow").place(x=170, y=170,width=220)
         cmb_gender = ttk.Combobox(self.root, textvariable=self.var_emp_gender,values=("Select", "Male", "Female","Other"), state="readonly",justify=CENTER, font=("goudy old style", 15))
-        cmb_gender.place(x=550, y=170, width=180)
+        cmb_gender.place(x=570, y=170, width=220)
         cmb_gender.current(0)
-        txt_contact = Entry(self.root, textvariable=self.var_emp_contact, font=("goudy old style", 15, "bold"), bg="lightyellow").place(x=1000, y=170,width=180)
+        txt_contact = Entry(self.root, textvariable=self.var_emp_contact, font=("goudy old style", 15, "bold"), bg="lightyellow").place(x=1020, y=170,width=220)
 
         ##----row2----##
         lbl_name = Label(self.root, text="Name", font=("goudy old style", 15, "bold"),bg="#898AA6").place(x=50, y=220)
-        lbl_dob = Label(self.root, text="DOB", font=("goudy old style", 15, "bold"), bg="#898AA6").place(x=450, y=220)
-        lbl_doj = Label(self.root, text="DOJ", font=("goudy old style", 15, "bold"), bg="#898AA6").place(x=900, y=220)
+        lbl_dob = Label(self.root, text="D.O.B", font=("goudy old style", 15, "bold"), bg="#898AA6").place(x=450, y=220)
+        lbl_doj = Label(self.root, text="D.O.J", font=("goudy old style", 15, "bold"), bg="#898AA6").place(x=900, y=220)
 
-        txt_name = Entry(self.root, textvariable=self.var_emp_name, font=("goudy old style", 15, "bold"),bg="lightyellow").place(x=150, y=220,width=180)
-        txt_dob = Entry(self.root, textvariable=self.var_emp_dob, font=("goudy old style", 15, "bold"),bg="lightyellow").place(x=550, y=220,width=180)
-        txt_doj = Entry(self.root, textvariable=self.var_emp_doj, font=("goudy old style", 15, "bold"),bg="lightyellow").place(x=1000, y=220,width=180)
+        txt_name = Entry(self.root, textvariable=self.var_emp_name, font=("goudy old style", 15, "bold"),bg="lightyellow").place(x=170, y=220,width=220)
+        txt_dob = Entry(self.root, textvariable=self.var_emp_dob, font=("goudy old style", 15, "bold"),bg="lightyellow").place(x=570, y=220,width=220)
+        txt_doj = Entry(self.root, textvariable=self.var_emp_doj, font=("goudy old style", 15, "bold"),bg="lightyellow").place(x=1020, y=220,width=220)
+
+        ##----row3----##
+        lbl_email = Label(self.root, text="Email", font=("goudy old style", 15, "bold"), bg="#898AA6").place(x=50, y=270)
+        lbl_pass = Label(self.root, text="Password", font=("goudy old style", 15, "bold"), bg="#898AA6").place(x=450, y=270)
+        lbl_utype = Label(self.root, text="User Type", font=("goudy old style", 15, "bold"), bg="#898AA6").place(x=900, y=270)
+
+        txt_email = Entry(self.root, textvariable=self.var_emp_email, font=("goudy old style", 15, "bold"),bg="lightyellow").place(x=170, y=270, width=220)
+        txt_pass = Entry(self.root, textvariable=self.var_emp_paswd, font=("goudy old style", 15, "bold"),bg="lightyellow").place(x=570, y=270, width=220)
+        cmb_utype = ttk.Combobox(self.root, textvariable=self.var_emp_utype,values=("Admin", "Employee"), state="readonly", justify=CENTER, font=("goudy old style", 15))
+        cmb_utype.place(x=1020, y=270, width=220)
+        cmb_utype.current(0)
+
+        ##-----row4----##
+        lbl_add = Label(self.root, text="Address", font=("goudy old style", 15, "bold"), bg="#898AA6").place(x=50, y=320)
+        lbl_salary = Label(self.root, text="Salary", font=("goudy old style", 15, "bold"), bg="#898AA6").place(x=570, y=320)
+
+        self.txt_add = Text(self.root, font=("goudy old style", 15, "bold"),bg="lightyellow")
+        self.txt_add.place(x=170, y=320, width=380,height=80)
+        txt_salary = Entry(self.root, textvariable=self.var_emp_salary, font=("goudy old style", 15, "bold"),bg="lightyellow").place(x=650, y=320, width=220)
+
+        ##----button---##
+        btn_save = customtkinter.CTkButton(self.root,text="Save",text_font=("groud old style",15,"bold"),bg_color="#898AA6",fg_color="blue",cursor="hand2").place(x=460,y=300,width=110,height=28)
+        btn_update = customtkinter.CTkButton(self.root, text="Update", text_font=("groud old style", 15, "bold"),bg_color="#898AA6",fg_color="green",cursor="hand2").place(x=560, y=300,width=110,height=28)
+        btn_delete = customtkinter.CTkButton(self.root,text="Delete",text_font=("groud old style",15,"bold"),bg_color="#898AA6",fg_color="red",cursor="hand2").place(x=660,y=300,width=110,height=28)
+        btn_clear = customtkinter.CTkButton(self.root, text="Clear", text_font=("groud old style", 15, "bold"),bg_color="#898AA6",fg_color="black",cursor="hand2").place(x=760, y=300,width=110,height=28)
+
+
+        ##---Emplyee Table---##
+
+        emp_frame = Frame(self.root,bd=3,relief=RIDGE)
+        emp_frame.place(x=0,y=420,relwidth=1,height=200)
+        scrolly = Scrollbar(emp_frame,orient=VERTICAL)
+        scrollx = Scrollbar(emp_frame, orient=HORIZONTAL)
+
+        self.EmpTable = ttk.Treeview(emp_frame,columns=("eid","name","email","gender","contact","dob","doj","pass","utype","address","salary"),yscrollcommand=scrolly.set,xscrollcommand=scrollx.set)
+        scrollx.pack(side=BOTTOM,fill=X)
+        scrolly.pack(side=RIGHT,fill=Y)
+        self.EmpTable.heading("eid",text="EMP ID")
+        self.EmpTable.heading("name", text="Name")
+        self.EmpTable.heading("email", text="Email")
+        self.EmpTable.heading("gender", text="Gender")
+        self.EmpTable.heading("contact", text="Contact")
+        self.EmpTable.heading("dob", text="D.O.B")
+        self.EmpTable.heading("doj", text="D.O.J")
+        self.EmpTable.heading("pass", text="Password")
+        self.EmpTable.heading("utype", text="User Type")
+        self.EmpTable.heading("address", text="Address")
+        self.EmpTable.heading("salary", text="Salary")
+
+        self.EmpTable["show"]="headings"
+
+        self.EmpTable.column("eid", width=90)
+        self.EmpTable.column("name", width=100)
+        self.EmpTable.column("email", width=100)
+        self.EmpTable.column("gender", width=100)
+        self.EmpTable.column("contact", width=100)
+        self.EmpTable.column("dob", width=100)
+        self.EmpTable.column("doj", width=100)
+        self.EmpTable.column("pass", width=100)
+        self.EmpTable.column("utype", width=100)
+        self.EmpTable.column("address", width=100)
+        self.EmpTable.column("salary", width=100)
+        self.EmpTable.pack(fill=BOTH,expand=1)
 
 
 if __name__ == "__main__":
